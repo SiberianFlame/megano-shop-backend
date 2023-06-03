852af775b0b0f1a506eee85dc1a9c281c8107fdc
Solovyov Viktor <amacuk2015@yandex.ru>
2023-06-03 23:23:05 +0300

    Сatalog & products are implemented

--- megano/api/urls.py
+++ megano/api/urls.py
@@ -2,23 +2,24 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('banners', views.banners),
+    path('banners', views.BannersAPIView.as_view()),
     path('categories', views.CategoryAPIView.as_view()),
-    path('catalog', views.catalog),
-    path('products/popular', views.productsPopular),
-    path('products/limited', views.productsLimited),
-    path('sales', views.sales),
-    path('basket', views.basket),
-    path('orders', views.orders),
+    path('catalog', views.CatalogViewSet.as_view({'get': 'list'})),
+    # path('catalog/<int:id>', views.ProductAPIView.as_view()),
+    path('products/popular', views.PopularProductsAPIView.as_view()),
+    path('products/limited', views.LimitedProductsAPIView.as_view()),
+    path('sales', views.SalesViewSet.as_view({'get': 'list'})),
+    path('basket', views.basket), #  todo add basket controller
+    path('orders', views.orders), # todo add orders controller
     path('sign-in', views.signIn),
     path('sign-up', views.signUp),
     path('sign-out', views.signOut),
-    path('product/<int:id>', views.product),
-    path('product/<int:id>/reviews', views.productReviews),
-    path('tags', views.tags),
+    path('product/<int:id>', views.ProductAPIView.as_view()),
+    path('product/<int:id>/reviews', views.ProductReviewsAPIView.as_view()),
+    path('tags', views.TagsAPIView.as_view()),
     path('profile', views.ProfileAPIView.as_view()),
     path('profile/password', views.ChangePasswordAPIView.as_view()),
     path('profile/avatar', views.AvatarAPIView.as_view()),
-    path('order/<int:id>', views.order),
-    path('payment/<int:id>', views.payment),
+    path('order/<int:id>', views.order), # todo add order controller
+    path('payment/<int:id>', views.payment), # todo add payment controller
 ]
